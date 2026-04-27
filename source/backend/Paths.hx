3c6a2c1a8e52e686f68a80d05b07bbd6b2f3f9cb
affsvoyo <githubunoficial@gmail.com>
2026-04-26 22:43:03 -0300

    Update Paths.hx

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -1,11 +1,25 @@
 package backend;
 
+import flixel.graphics.frames.FlxAtlasFrames;
+import openfl.utils.Assets;
+
 class Paths {
     public static function image(key:String):String {
         return 'assets/images/' + key + '.png';
     }
 
+    public static function xml(key:String):String {
+        return 'assets/images/' + key + '.xml';
+    }
+
     public static function sound(key:String):String {
         return 'assets/sounds/' + key + '.ogg';
+    }
+
+    public static function getSparrowAtlas(key:String):FlxAtlasFrames {
+        return FlxAtlasFrames.fromSparrow(
+            image(key),
+            Assets.getText(xml(key))
+        );
     }
 }
