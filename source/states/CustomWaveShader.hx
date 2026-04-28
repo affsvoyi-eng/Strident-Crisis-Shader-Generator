7fb3e018c1190280e79fc2d4da4b284f5ca10209
affsvoyo <githubunoficial@gmail.com>
2026-04-28 15:14:31 -0300

    Refactor CustomWaveShader for clarity and efficiency

--- source/states/CustomWaveShader.hx
+++ source/states/CustomWaveShader.hx
@@ -1,50 +1,51 @@
 package states;
 
 import flixel.system.FlxAssets.FlxShader;
-import openfl.utils.Assets;
 
 class CustomWaveShader extends FlxShader
 {
-    @:glFragmentSource('
-        #pragma header
+@:glFragmentSource('
+#pragma header
+//uniform float tx, ty; // x,y waves phase
+uniform float uTime;
 
-        uniform float uTime;
-        uniform float uSpeed;
-        uniform float uFrequency;
-        uniform float uWaveAmplitude;
+/**  
+	 * How fast the waves move over time  
+	 */  
+	uniform float uSpeed;  
+	  
+	/**  
+	 * Number of waves over time  
+	 */  
+	uniform float uFrequency;  
+	  
+	/**  
+	 * How much the pixels are going to stretch over the waves  
+	 */  
+	uniform float uWaveAmplitude;  
 
-        vec2 sineWave(vec2 pt)
-        {
-            float x = 0.0;
-            float y = 0.0;
+	vec2 sineWave(vec2 pt)  
+	{  
+		float x = 0.0;  
+		float y = 0.0;  
+		  
+			float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;  
+            float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;  
+			pt.y += offsetY;  
+			pt.x += offsetX;  
+		return vec2(pt.x + x, pt.y + y);  
+	}  
 
-            float offsetY = sin(pt.y * uFrequency + 10.0 * pt.x + uTime * uSpeed) * uWaveAmplitude;
-            float offsetX = sin(pt.x * uFrequency + 5.0 * pt.y + uTime * uSpeed) * uWaveAmplitude;
+	void main()  
+	{  
+		vec2 uv = sineWave(openfl_TextureCoordv);  
+		gl_FragColor = texture2D(bitmap, uv);
 
-            pt.y += offsetY;
-            pt.x += offsetX;
+}
+')
 
-            return vec2(pt.x + x, pt.y + y);
-        }
-
-        void main()
-        {
-            vec2 uv = sineWave(openfl_TextureCoordv);
-            gl_FragColor = texture2D(bitmap, uv);
-        }
-    ')
-
-    public function new()
-    {
-        super();
-
-        #if !macro
-        var frag:String = Assets.getText("assets/data/shader/Wavy.frag");
-
-        if (frag != null && frag.length > 0)
-        {
-            __glFragmentSource = frag;
-        }
-        #end
-    }
+public function new()  
+{  
+    super();  
+}
 }
