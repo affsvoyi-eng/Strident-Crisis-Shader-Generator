9008f1a2b98ba8d755df4e4ee8f4cb6d5d109720
affsvoyo <githubunoficial@gmail.com>
2026-05-05 21:07:19 -0300

    Update IntroState.hx

--- source/states/IntroState.hx
+++ source/states/IntroState.hx
@@ -23,40 +23,11 @@ class IntroState extends FlxState
 
     function decideNextState():Void
     {
-        var configured:Bool = false;
-
-        #if mobile
         if (FlxG.save.data.configured != null && FlxG.save.data.configured == true)
         {
             configured = true;
         }
-
-        #elseif android
-        var bootPath:String = "assets/data/firstboot.txt";
-
-        if (Assets.exists(bootPath))
-        {
-            var content:String = Assets.getText(bootPath);
-
-            if (content != null && content.indexOf("configured=true") != -1)
-            {
-                configured = true;
-            }
-        }
-
-        #elseif sys
-        var bootPath:String = "assets/data/firstboot.txt";
-
-        if (FileSystem.exists(bootPath))
-        {
-            var content:String = File.getContent(bootPath);
-
-            if (content != null && content.indexOf("configured=true") != -1)
-            {
-                configured = true;
-            }
-        }
-        #end
+    }
 
         if (configured)
         {
